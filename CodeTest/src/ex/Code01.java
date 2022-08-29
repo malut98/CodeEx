8b8b68e1efee20715d170f9436588f432c6e032c
malut98 <aokimki1@naver.com>
2022-08-29 13:29:37 +0900

    test

--- CodeTest/src/ex/Code01.java
+++ CodeTest/src/ex/Code01.java
@@ -1,10 +1,35 @@
 package ex;
 
 public class Code01 {
+	private int a;
+	private String b;
+	
+	public Code01() {}
 
-	public static void main(String[] args) {
-		System.out.println("a");
-
+	public Code01(int a, String b) {
+		super();
+		this.a = a;
+		this.b = b;
 	}
 
+	public int getA() {
+		return a;
+	}
+
+	public void setA(int a) {
+		this.a = a;
+	}
+
+	public String getB() {
+		return b;
+	}
+
+	public void setB(String b) {
+		this.b = b;
+	}
+	
+	public void total() {
+		
+	}
+	
 }
---
+++ CodeTest/src/ex/Main.java
@@ -0,0 +1,10 @@
+package ex;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Code01 c01 = new Code01();
+		c01.total();
+	}
+
+}
